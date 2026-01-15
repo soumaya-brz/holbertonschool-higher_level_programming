bc563dcfb7f1bab139e239e0f9a3e403d78eb211
soumaya <soumayabraziii@gmail.com>
2026-01-15 14:46:55 +0000

    fix: correction for pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i != 0 or j != 0:
-            print("{:01}{:01}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
+        print("{:01}{:01}".format(i, j),
+              end=", " if i != 8 or j != 9 else "\n")
