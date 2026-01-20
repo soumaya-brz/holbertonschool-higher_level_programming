4f8da0e677af8803fc9153087d829080051749e5
soumaya <soumayabraziii@gmail.com>
2026-01-20 10:04:45 +0000

    fix: handle subtractive notation

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,14 @@ def roman_to_int(roman_string):
     roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
     total = 0
+    prev_value = 0
 
-    for char in roman_string:  # parcours normal (de gauche à droite)
-        total += roman_map[char]
+    for char in reversed(roman_string):
+        value = roman_map[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
 
     return total
