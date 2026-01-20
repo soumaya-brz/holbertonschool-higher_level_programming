a41cd22fdfcbb26813020ebf7149a61a5efd7db7
soumaya <soumayabraziii@gmail.com>
2026-01-20 09:31:28 +0000

    fix: handle None input in best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
     best_key = None
     max_value = None
     for key, value in a_dictionary.items():
