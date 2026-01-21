af44dfc47c89bc2ad826eb6ed2c98ae5e56e8259
soumaya <soumayabraziii@gmail.com>
2026-01-21 21:45:46 +0000

    fix: correction

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except ZeroDivisionError:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
