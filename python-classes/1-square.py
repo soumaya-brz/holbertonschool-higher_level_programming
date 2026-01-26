895c17a9f5db5555a2651acf9e5378b7bf896317
soumaya <soumayabraziii@gmail.com>
2026-01-26 11:07:23 +0000

    feat: defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square instance
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
