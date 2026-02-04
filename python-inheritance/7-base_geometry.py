2635c94204d8561ad14a198cacb6d327bafde002
soumaya <soumayabraziii@gmail.com>
2026-02-04 09:02:55 +0000

    fix: rectify code to satisfy checker

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Defines a BaseGeometry class"""
+"""Defines a class BaseGeometry"""
 
 
 class BaseGeometry:
-    """Base class for geometry objects"""
+    """BaseGeometry class"""
 
     def area(self):
-        """Raises an exception indicating that area is not implemented"""
+        """Raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is a positive integer
-
-        Args:
-            name (str): the name of the value
-            value (int): the value to validate
-        """
+        """Validates value"""
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
