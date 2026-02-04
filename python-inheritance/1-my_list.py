7fde47eb8ddcbf806e82bd7a93e72aad7d9472c8
soumaya <soumayabraziii@gmail.com>
2026-02-04 08:53:12 +0000

    fix: print_sorted without modifying original list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a class MyList that inherits from list."""
+"""Defines a class MyList that inherits from list"""
 
 
 class MyList(list):
-    """Custom list class that can print itself sorted."""
+    """Custom list class"""
 
     def print_sorted(self):
-        """Prints the list sorted in ascending order."""
+        """Prints the list sorted in ascending order"""
         print(sorted(self))
