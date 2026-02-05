39084731a2bbfbdbe06ae307968a527f41f2f402
soumaya <soumayabraziii@gmail.com>
2026-02-05 09:23:04 +0000

    feat: add VerboseList with notifications on list operations

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module that defines a VerboseList class
+"""
+
+
+class VerboseList(list):
+    """List subclass that prints notifications on modifications"""
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
