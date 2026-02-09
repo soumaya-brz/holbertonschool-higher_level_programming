fcd5ff89f7e509827b3250960ed890887c838ac5
soumaya <soumayabraziii@gmail.com>
2026-02-09 10:18:29 +0000

    feat: add function to read and print a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that contains a function to read and print a text file."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
