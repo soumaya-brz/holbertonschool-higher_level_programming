ab31938cc1766650d51b181315137f01fd8fb2a8
soumaya <soumayabraziii@gmail.com>
2026-03-05 23:00:48 +0000

    fix: list genres of Dexter with correct join

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,6 @@ FROM tv_genres
 JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
+ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC;
